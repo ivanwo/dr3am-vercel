25f099c550c2acc5d1b3779a68f1a9088c9d07c7
ivan wolyniec <ivanwolyniec@gmail.com>
2022-07-08 17:17:52 -0400

    started infrastructure for B2C and created tenant in azure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+} from "@azure/msal-react";
 import React from "react";
 
 const App = () => {
-  return <h1>D.S.</h1>;
+  return (
+    <>
+      <h1>D.S.</h1>
+      <AuthenticatedTemplate>
+        <p>in</p>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <p>out</p>
+      </UnauthenticatedTemplate>
+    </>
+  );
 };
 
 export default App;
