031f3abe2ca693909acd9f2ea8d06bc780a67fd9
ivan wolyniec <ivanwolyniec@gmail.com>
2022-07-11 10:39:04 -0400

    refinement

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import {
   AuthenticatedTemplate,
   UnauthenticatedTemplate,
   useMsal,
 } from "@azure/msal-react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { loginRequest } from "../settings/msalConfig";
 
 const App = () => {
   const { instance } = useMsal();
   const { accounts } = useMsal();
-//   instance.acquireTokenSilent()
+  //   instance.acquireTokenSilent()
   // Inside the Router, we have two paths beneath the header
   // Routes for when the user is authenticated, and Routes for them they're not.
   return (
@@ -18,56 +18,108 @@ const App = () => {
       <h1>D.S.</h1>
       <AuthenticatedTemplate>
         <Routes>
-          <Route path="/" element={<LoggedIn instance={instance} accounts={accounts} />}></Route>
+          <Route
+            path="/"
+            element={<LoggedIn instance={instance} accounts={accounts} />}
+          ></Route>
         </Routes>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
         <Routes>
-          <Route path="/" element={<LoggedOut instance={instance} accounts={accounts} />}></Route>
+          <Route
+            path="/"
+            element={<LoggedOut instance={instance} accounts={accounts} />}
+          ></Route>
         </Routes>
       </UnauthenticatedTemplate>
+      <FooterNav instance={instance} accounts={accounts}></FooterNav>
     </BrowserRouter>
   );
 };
 
-const LoggedIn = ({instance, accounts}) => {
-    // useEffect(_ => {
-    //     instance.acquireTokenSilent({
-    //         ...loginRequest(""),
-    //         account: accounts[0],
-    //       }).then(res => console.log(res))
-    // }, []);
+const LoggedIn = ({ instance, accounts }) => {
+  // useEffect(_ => {
+  //     instance.acquireTokenSilent({
+  //         ...loginRequest(""),
+  //         account: accounts[0],
+  //       }).then(res => console.log(res))
+  // }, []);
 
-    let sendWebRequest = _ => {
-        instance.acquireTokenSilent({
-            ...loginRequest(),
-            account: accounts[0],
-          }).then(response => {
-            let bearerHeader = `bearer ${response.accessToken}`;
-            // send fetch request with bearerHeader as "Authorization" header
-            // possibly, Content-Type : application/json
-            // i'm interested in how the SA integration could go w/ file access, might need a 
-            // wrapper for that. or even better, an APIM policy?
-          })
-    }
+  let sendWebRequest = (_) => {
+    instance
+      .acquireTokenSilent({
+        // TODO: move scope get back to auth config?
+        // TODO: determine scope for SA access tokens
+        // TODO: enable api access on SA from app registration side
+        scopes: ["OpenID"],
+        // scopes: ["User.Read"],
+        account: accounts[0],
+      })
+      .then((response) => {
+        let bearerHeader = `bearer ${response.accessToken}`;
+        console.log(response);
+        // send fetch request with bearer token as "Authorization" header
+        // possibly, Content-Type : application/json
+        // i'm interested in how the SA integration could go w/ file access, might need a
+        // wrapper for that. or even better, an APIM policy?
+      });
+  };
   return (
     <>
-      <p>logged in as {accounts[0]?.username}</p>
-      <button onClick={(_) => instance.logoutPopup({ postLogoutRedirectUri: "/"})}>log out</button>
-      <button onClick={_ => console.log(accounts[0])}>who me</button>
-      <button onClick={_ => sendWebRequest()}>web request</button>
-      <p id="footer">footer</p>
+      <p>logged in as {accounts[0]?.idTokenClaims.given_name}</p>
+
+      <button onClick={(_) => console.log(instance.getAllAccounts())}>
+        who me
+      </button>
+      <button onClick={(_) => sendWebRequest()}>web request</button>
     </>
   );
 };
 
-const LoggedOut = ({instance, accounts}) => {
+const LoggedOut = ({ instance, accounts }) => {
   return (
     <>
       <p>logged out</p>
-      <button onClick={_ => instance.loginPopup(loginRequest)}>log in</button>
     </>
   );
+};
+
+// TODO: conditional options based on if the user is logged in or not
+const FooterNav = ({ instance, accounts }) => {
+    // more options for a logged in user in the nav menu
+  if (accounts[0])
+    return (
+      <nav id="navfooter">
+        <Link to="about" className="footerlink">
+          about
+        </Link>
+        <Link to="user" className="footerlink">
+          {accounts[0]?.idTokenClaims.given_name}'s dreams
+        </Link>
+        <Link to="new" className="footerlink">
+          new dream
+        </Link>
+        <a
+          className="footerlink"
+        //   onClick={(_) => instance.logoutPopup({ postLogoutRedirectUri: "/" })}
+          onClick={(_) => instance.logoutRedirect({ postLogoutRedirectUri: "/" })}
+        >
+          log out
+        </a>
+      </nav>
+    );
+    // not logged in, not many option
+  else
+    return (
+      <nav id="navfooter">
+        <a
+          onClick={(_) => instance.loginPopup(loginRequest)}
+          className="footerlink"
+        >
+          log in
+        </a>
+      </nav>
+    );
 };
 
 export default App;
